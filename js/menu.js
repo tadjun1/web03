ec4c05143880163966d0355419408dcb7c61e532
tadjun1 <72842877+tadjun1@users.noreply.github.com>
2020-10-19 08:14:55 +0900

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+(function($){
+  $(function(){
+   
+  var $header = $('header'),
+      $menu = $header.find('.menu-btn'),
+      $gnb = $header.find('.gnb');
+   
+  $menu.on('click', function(){
+    $menu.toggleClass('on');
+    $gnb.toggleClass('on');
+  });  
+  
+  $gnb.on('click', 'a', function(){
+		$menu.toggleClass('on');
+    $gnb.toggleClass('on');
+  });
+   
+  $(window).on('resize', function(){
+    $menu.removeClass('on');
+    $gnb.removeClass('on')
+  }) 
+    
+  });
+})(jQuery);
+
+
+
+
